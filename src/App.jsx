9aa5d7d0f34d15070f7f1600757cecc5195c7435
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2024-12-08 17:36:40 +0500

    "add tailwind to project "

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
- 
 
 function App() {
  
   return (
     <>
      <h1>hellow hy iam therw</h1>
-     <h1 className="text-3xl  text-yellow-500 bg-white font-bold underline">
+     <h1 className="text-3xl  text-yellow-500 bg-black font-bold underline">
       Hello world!
     </h1>
     </>
