5ee0a46f39e14cea9ce9ec1d811dbd37dda8dab7
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2025-01-02 15:52:55 +0500

    "add"

--- src/Components/TodoForm.jsx
+++ src/Components/TodoForm.jsx
@@ -8,7 +8,7 @@ function TodoForm() {
               placeholder="Write Todo..."
               className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
           />
-          <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+          <button  onClick={(e)=> e.preventDefault()} type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
               Add
           </button>
       </form>
